ebbbc754a55a4a51db91b5ffb294078ebd6795b7
gaaanesh <ganesh.badhe5@gmail.com>
2020-09-27 13:35:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,138 @@
+// var a = '10';
+// var b = '5';
+// var c = '20';
+// var d = '30';
+// var total = Number(a)+Number(b)+Number(c)+Number(d)
+// var result = Math.ceil(total / 4);
+
+// console.log(result)
+
+// var randomDieRollOutcome = Math.floor(
+//     Math.random() * 6
+// ) + 1;
+
+// console.log(randomDieRollOutcome)
+    
+
+// function extractNumber (a){
+//     return a.substr(4)
+// }
+
+// extractNumber('GSX-444')
+// extractNumber('GSX-9867990')
+
+
+// function evaluateName(sex, name) {
+// 	if (name) {
+// 		return name;
+// 	}
+// 	return (sex === 'm' ? 'John Doe' : 'Jane Doe');
+// }
+
+// evaluateName('f', 'John Doe');
+
+
+
+// function isEven (number){
+//     if ((number % 2) === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// isEven (4);
+
+
+// function getExamResult(score){
+// if (score > 100) {
+//     return "Cheat"
+// }
+// }
+
+
+
+// function count() {
+// 	var total = 0;
+// 	for (var i = 1; i <= 5; i = i + 2) {
+// 		total = total + (i * 10);
+// 	}
+// 	return total;
+// }
+
+// 10, 30, 50
+
+
+
+
+// function add(a, b) {
+// 	return a.concat(b);
+// }
+
+// add([1,2,3], ['a', 'b', 'c']);
+
+
+
+
+
+
+// var myList = ['a', 'b', 'c', 'd'];
+// function remove(list, i) {
+//     var removeFromList = myList.splice(i, 1)
+// }
+
+
+
+// var myList = ['Java', 'PHP', 'Ruby'];
+// function reversed(list) {
+//     list.reverse();
+//     return list
+// }
+
+
+var first = 'a';
+var second = 'b';
+var joined = first.concat(second)
+
+
+// var part1 = "Javascript";
+// var part2 = "is";
+// var part3 = "cool";
+// var message = part1 + ' ' + part2 + ' ' + part3
+
+// console.log(message.toUpperCase())
+
+
+// var greeting = "Hello";
+// console.log(greeting.toUpperCase())
+
+
+// console.log(window.location.pathname.split('/'));
+
+// var ourCode = "GSX-116";
+// console.log(ourCode.replace('GSX-', ''))
+
+// var ourCode = "GSX-116";
+// console.log(ourCode.substr(4))
+
+
+
+
+
+
+
+// console.log('min', Math.min(0.9, 4.8, 1.5))
+// console.log('max', Math.max(0.9, 4.8, 1.5))
+
+// console.log("0.9", Math.ceil(0.9))
+// console.log("4.8", Math.ceil(4.8))
+// console.log("1.5", Math.ceil(1.5))
+
+// console.log("0.9", Math.floor(0.9))
+// console.log("4.8", Math.floor(4.8))
+// console.log("1.5", Math.floor(1.5)) 
+
+// console.log(Math.floor(Math.random() * 10));
+
+
+
